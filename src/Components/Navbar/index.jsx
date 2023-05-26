70599761921a7be61b210dd061e4d15a84faba24
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2023-05-26 14:45:00 -0500

    feat: :construction: Add feature navbar & sign in
    Add feature that shows certain elements in the Navbar depending on the value of the signout key in LocalStorage and create Sign In page

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -9,12 +9,70 @@ const Navbar = () => {
   const context = useContext(ShoppingCartContext);
   const activeStyle = 'underline underline-offset-8';
 
+  const signOut = localStorage.getItem('sign-out')
+  const parsedSignOut = JSON.parse(signOut)
+  const isUserSignOut = context.signOut || parsedSignOut
+
   const handleSignOut = () => {
     const stringifiedSignOut = JSON.stringify(true)
     localStorage.setItem('sign-out', stringifiedSignOut)
     context.setSinOut(true)
   }
 
+  const renderView = () => {
+    if (isUserSignOut) {
+      return (
+        <li>
+          <NavLink 
+            to='/sign-in'
+            className={({ isActive }) => 
+            isActive ? activeStyle : undefined  
+            }
+            onClick={handleSignOut}
+          >
+            Sign In
+          </NavLink>
+        </li>
+      )
+    } else {
+      return (
+        <>
+          <li className="text-black/60">
+          juancodeaudio@platzi.com
+        </li>
+        <li>
+          <NavLink 
+            to='/my-orders'
+            className={({ isActive }) => 
+            isActive ? activeStyle : undefined  
+          }>
+            My Orders
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to='/my-account'
+            className={({ isActive }) => 
+            isActive ? activeStyle : undefined  
+          }>
+            My Account
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to='/sign-in'
+            className={({ isActive }) => 
+            isActive ? activeStyle : undefined  
+            }
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </NavLink>
+        </li>
+        </>
+      )
+    }
+  }
   return (
     <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
       <ul className="flex items-center gap-3">
@@ -85,38 +143,7 @@ const Navbar = () => {
         </li>
       </ul>
       <ul className="flex items-center gap-3">
-        <li className="text-black/60">
-          juancodeaudio@platzi.com
-        </li>
-        <li>
-          <NavLink 
-            to='/my-orders'
-            className={({ isActive }) => 
-            isActive ? activeStyle : undefined  
-          }>
-            My Orders
-          </NavLink>
-        </li>
-        <li>
-          <NavLink 
-            to='/my-account'
-            className={({ isActive }) => 
-            isActive ? activeStyle : undefined  
-          }>
-            My Account
-          </NavLink>
-        </li>
-        <li>
-          <NavLink 
-            to='/sign-in'
-            className={({ isActive }) => 
-            isActive ? activeStyle : undefined  
-            }
-            onClick={handleSignOut}
-          >
-            Sign Out
-          </NavLink>
-        </li>
+        {renderView()}
         <li onClick={context.openCheckoutSideMenu} className="flex items-center cursor-pointer">
           <ShoppingBagIcon className="h-6 w-6 text-black" /> 
           <div>{context.cartProducts.length}</div>
