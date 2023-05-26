7ef6c16c133991d3d6b455197eb3a2934ea4361e
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2023-05-26 15:06:18 -0500

    feat: :construction: Add LocalState for sign in options
    Add LocalState to show the create account view or the sign in view

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,10 +1,11 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ShoppingCartContext } from "../../Context"
 import { Link } from "react-router-dom"
 import Layout from "../../Components/Layout"
 
 function SignIn() {
   const context = useContext(ShoppingCartContext)
+  const [view, setView] = useState('user-info')
 
   const account = localStorage.getItem('account')
   const parsedAccount = JSON.parse(account)
@@ -12,9 +13,9 @@ function SignIn() {
   const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
   const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
-  return (
-    <Layout>
-      <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+
+  const renderLogIn = () => {
+    return (
       <div className="flex flex-col w-80">
         <p>
           <span className="font-light text-sm">Email: </span>
@@ -37,11 +38,25 @@ function SignIn() {
         </div>
         <button 
           className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3"
+          onClick={() => setView('create-user-info')}
           disabled={hasUserAnAccount}  
         >
           Sign Up
         </button>
       </div>
+    )
+  }
+
+  const renderCreateUserInfo = () => {
+    //...
+  }
+
+  const renderView = () => view === 'create-user-info' ? renderCreateUserInfo() : renderLogIn()
+
+  return (
+    <Layout>
+      <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+      {renderView()}
     </Layout>
   )
 }
